ce5085a1bd52b8bf9f24e39061f8c044682c3a21
lati111 <jvzelst111@gmail.com>
2023-04-22 19:02:02 +0200

    Revamped youtube downloading system

--- scripts/playlistDownloader.py
+++ scripts/playlistDownloader.py
@@ -1,44 +1,36 @@
-import os, re ,csv, sys
-import requests, shutil
-from csv import writer
+import os ,csv, sys, string, random, shutil
 
 from tkinter import *
 from tkinter.ttk import *
 
 from pytube import Playlist
-from pytube import YouTube
 
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, error
+import yt_dlp
 
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-import eyed3
-from eyed3.id3.frames import ImageFrame
 
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
 
 def enablePrint():
     sys.stdout = sys.__stdout__
-
-def cleanupFolder(folder):
     next = True
     
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        while next:
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-                next = True
-            except Exception as e:
-                next = False
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
         
 class PlaylistDownloader():
+    SAVE_PATH = os.getcwd() + '/backup'
+    PLAYLIST_PATH = os.getcwd() + '/playlists'
+    TEMP_PATH = os.getcwd() + '/processing'
+    MUSIC_PATH = os.getcwd() + '/music'
+    playlistTitle = ""
     counterText = None
+    current = 0
+    total = 0
     urlInput = None
     playlists = []
     gui = None
@@ -51,91 +43,116 @@ class PlaylistDownloader():
             for row in reader:
                 self.playlists.append(row)
     
-    def downloadVideo(self, video, destination_path):
-        vidTitle = ""
-        while vidTitle == "":  
-            try:
-                vidTitle = video.title
-            except (KeyError, Exception, error):
-                vidTitle = video.streams[0].title
-                pass
-        vidTitle = re.sub('[^a-zA-Z0-9 \n\.]', '', vidTitle)
-        vidTitle = re.sub(' +', ' ', vidTitle)
-        mp4_path = "processing/"+vidTitle+".mp4"
-        mp3_path = destination_path + "/" + vidTitle + ".mp3"
-        
-        # download mp4
-        if not os.path.isfile(mp3_path):
-            video.streams.filter(progressive=True, file_extension='mp4').first().download("processing/", vidTitle+".mp4", None, 1)
-            
-            # download cover
-            img_path = "processing/"+vidTitle+".jpg"
-            img_data = requests.get(video.thumbnail_url).content
-            with open(img_path, 'wb') as handler:
-                handler.write(img_data)
-            
-            # convert to mp3
-            blockPrint()
-            video = VideoFileClip(os.path.join(mp4_path))
-            video.audio.write_audiofile(os.path.join(mp3_path))
-            enablePrint()
-            
-            # set cover
-            song = eyed3.load(mp3_path)
-            if (song.tag == None):
-                song.initTag()
-            song.tag.images.set(ImageFrame.FRONT_COVER, open(img_path,'rb').read(), 'image/jpeg')
-            song.tag.save(version=eyed3.id3.ID3_V2_3)
-            
-            audio = MP3(mp3_path, ID3=ID3)
-            try:
-                audio.add_tags()
-            except error:
-                pass
-            audio.tags.add(
-                APIC(encoding=3, mime='image/jpeg', type=3, desc=u'Cover', data=open(img_path, "rb").read())
-            )
-            audio.save()
+    def progressCounter(self, prefix):
+        self.current = self.current + 1
+        self.counterText.set(prefix + " " + str(self.current) +' / '+str(self.total))
+        self.gui.window.update() 
+    
+    def processYoutubeVideo(self, d):
+        if d['status'] == 'finished':
+            self.progressCounter("downloading")
                 
-    def downloadPlaylist(self, title, url):
+    def downloadPlaylist(self, title, url, source):
         # initialise GUI
         self.gui.clearWindow()
         Label(self.gui.window, text = 'Downloading Playlist: '+ title, font =('Verdana', 15)).pack(side = TOP, pady = 10, expand=True)
         print("Downloading playlist: " + title + "...")
+        self.playlistTitle = title
         
         # load playlist
         playlist = Playlist(url)
         playlist_path = "playlists/"+title
-        songAmount = len(playlist.video_urls)
-        self.counterText.set('0 / '+str(songAmount))
+        self.total = len(playlist.video_urls)
+        self.counterText.set('downloading 0 / '+str(self.total))
         Label(self.gui.window, textvariable= self.counterText, font =('Verdana', 10)).pack(side = TOP, pady = 10)
+        self.gui.window.update() 
         
         # check if playlist directory exists
         if not os.path.isdir(playlist_path):
             os.makedirs(playlist_path)
         
         # download songs
-        songCount = 0
-        for video in playlist.videos:
-            songCount = songCount + 1
-            self.downloadVideo(video, playlist_path)
-            self.counterText.set(str(songCount) + ' / '+str(songAmount))   
-            self.gui.window.update() 
+        match source:
+            case "youtube":
+                self.downloadYoutubePlaylist(url)
+                
+        # convert to mp3s
+        print("converting to mp3")
+        self.current = 0
+        self.convertPlaylist()
         
-        self.counterText.set("Cleaning up files...")
-        self.gui.window.update() 
-        cleanupFolder("processing")
         print("playlist downloaded")
-        
         self.gui.openPlaylistDownloader()
-            
-    def createPlaylistOption(self, title, url):
-        Button(self.gui.window, text = title, width='40', command= lambda: self.downloadPlaylist(title, url)).pack(side = TOP, padx= 20)
-    
+          
+    def downloadYoutubePlaylist(self, url):
+        self.SAVE_PATH = os.getcwd() + '/backup/' + self.playlistTitle
+        self.PLAYLIST_PATH = os.getcwd() + '/playlists/' + self.playlistTitle
+        self.TEMP_PATH = os.getcwd() + '/processing/' + self.playlistTitle
+        
+        ydl_opts = {
+            'outtmpl': self.SAVE_PATH + '/%(title)s.%(ext)s',
+            'format': 'mp4',
+            'progress_hooks': [self.processYoutubeVideo],
+            'writethumbnail': True,
+            'ignoreerrors': True,
+            'no_warnings': True,
+            'quiet': True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(url)
+      
+    def convertPlaylist(self):
+        for filename in os.listdir(self.SAVE_PATH):
+            f = os.path.join(self.SAVE_PATH, filename)
+            if os.path.isfile(f):
+                if ".mp4" not in filename:
+                    continue
+                
+                mp3_path = os.path.join(self.PLAYLIST_PATH,filename).replace(".mp4", ".mp3")
+                if os.path.isfile(mp3_path):
+                    continue
+                
+                tempName = get_random_string(12)
+                oldMp4Path = os.path.join(self.TEMP_PATH, filename)
+                newMp4Path = os.path.join(self.TEMP_PATH, tempName + ".mp4")
+                newMp3Path = os.path.join(self.TEMP_PATH,tempName + ".mp3")
+                if not os.path.isdir(self.TEMP_PATH):
+                    os.mkdir(self.TEMP_PATH)
+                    
+                shutil.copy2(f, oldMp4Path, follow_symlinks=False)
+                os.rename(oldMp4Path, newMp4Path)
+                
+                blockPrint()
+                video = VideoFileClip(newMp4Path)
+                video.audio.write_audiofile(newMp3Path)
+                enablePrint()
+                
+                os.rename(newMp3Path, os.path.join(self.PLAYLIST_PATH,filename.replace(".mp4", ".mp3")))
+                
+                self.progressCounter('converting')
+                
+    def createPlaylistOption(self, title, url, source):
+        match source:
+            case "youtube":
+                title = title + " (youtube)"
+                Button(
+                    self.gui.window, 
+                    text = title, 
+                    width='40', 
+                    command= 
+                    lambda: self.downloadPlaylist(title, url, "youtube")
+                ).pack(side = TOP, padx= 20)
+ 
     def addNewPlaylist(self):
         url = self.urlInput.get()
         playlist = Playlist(url)
-        List = [playlist.title, url]
+        source = ""
+        
+        if "https://www.youtube.com" in url:
+            source = "youtube"
+            
+        List = [playlist.title, url, source]
         self.playlists.append(List)
         
         with open(r"data\playlists.csv", 'a', newline='\n') as f_object:
@@ -149,7 +166,7 @@ class PlaylistDownloader():
     def openNewPlaylist(self):
         self.gui.clearWindow()
         self.gui.insertBanner("New Playlist", self.openPlaylistList)
-        Label(self.gui.window, text = 'Please link your youtube playlist', font =('Verdana', 10)).pack(side = TOP, pady = 10)
+        Label(self.gui.window, text = 'Please link your playlist', font =('Verdana', 10)).pack(side = TOP, pady = 10)
         self.urlInput = Entry(self.gui.window, width='45')
         self.urlInput.pack(side = TOP, padx = 20, pady=10, expand=True)
         Button(self.gui.window, text = "Add", width='15', command=self.addNewPlaylist).pack(side = TOP, padx= 40, pady = 10)
@@ -161,7 +178,7 @@ class PlaylistDownloader():
         Button(self.gui.window, text = "New Playlist", width='25', command=self.openNewPlaylist).pack(side = TOP, padx= 40)
         
         for playlist in self.playlists[1:]:
-            self.createPlaylistOption(playlist[0], playlist[1])
+            self.createPlaylistOption(playlist[0], playlist[1], playlist[2])
     
     def guiStart(self, gui):
         self.gui = gui
